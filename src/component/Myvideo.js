fd9b2c128f9831664d7c93fa570ee09e4a1affb8
adityarajpoot44 <adityakumar8279@gmail.com>
2024-10-12 12:54:01 +0530

    video background

--- src/component/Myvideo.js
+++ src/component/Myvideo.js
@@ -1,11 +1,13 @@
-import React from 'react'
+import React from 'react';
 import videoBg from '../assest/video/myVideo.mp4';
 
 function Myvideo() {
+
     return (
         <div className='video_main brightness-[40%] z-0'>
-            <video src={videoBg} loop autoPlay muted={true}  className='react_player'/>
-            </div>
+            <video src={videoBg} autoPlay muted={true} className='react_player' />
+            
+        </div>
     );
 }
 
--- src/component/pages/Home.js
+++ src/component/pages/Home.js
@@ -1,19 +1,26 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faInstagram, faGithub, faLinkedinIn, faTwitter} from '@fortawesome/free-brands-svg-icons'
+import { faInstagram, faGithub, faLinkedinIn, faTwitter,} from '@fortawesome/free-brands-svg-icons'
+// import { faVolumeHigh } from '@fortawesome/free-solid-svg-icons';
+// import { useState } from 'react';
 
 
 function Home() {
+
+    // const [icon,seticon]=useState(faVolumeHigh)
+    // seticon()
     return (
         <>
         <div className="home_center absolute z-20 ">
             <h1 className=" text-[70px] font-bold mb-[34px] md:w-[560px] tracking-[5px]">Aditya Rajpoot</h1>
             <p className="text-center text-[30px] tracking-wide"> I'm a <span>Developer</span></p>
         </div>
+
         <div className="absolute z-20 bottom-3 right-2 m-3 flex flex-col-reverse gap-5 ">
-                <a href="https://github.com/adityarajpoot44" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faGithub} className="mx-2 text-xl hover_social  hover:text-[#009e66] hover:text-3xl relative " /></a>
-                <a href="https://www.linkedin.com/in/aditya-rajpoot44/" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faLinkedinIn} className="mx-2 text-xl hover_social  hover:text-[#009e66] hover:text-3xl" /></a>
-                <a href="https://www.instagram.com/aditya_rajpoot_44/" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faInstagram} className="mx-2 text-xl hover_social  hover:text-[#009e66] hover:text-3xl" /></a>
-                <a href="https://twitter.com/adityar_44" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faTwitter} className="mx-2 text-xl hover_social hover:text-[#009e66] hover:text-3xl" /></a>
+                <a href="https://github.com/adityarajpoot44" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faGithub} className="mx-2 text-xl hover_social  hover:text-[#009e66] relative " /></a>
+                <a href="https://www.linkedin.com/in/aditya-rajpoot44/" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faLinkedinIn} className="mx-2 text-xl hover_social  hover:text-[#009e66]" /></a>
+                <a href="https://www.instagram.com/aditya_rajpoot_44/" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faInstagram} className="mx-2 text-xl hover_social  hover:text-[#009e66]" /></a>
+                <a href="https://twitter.com/adityar_44" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faTwitter} className="mx-2 text-xl hover_social hover:text-[#009e66]" /></a>
+                {/* <FontAwesomeIcon icon={icon} className="mx-2 text-xl hover_social hover:text-[#009e66]" /> */}
             </div>
         </>
     );
