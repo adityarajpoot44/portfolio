3eb4f7e0386929df9ddec58a4814c9b55a835c2e
adityarajpoot44 <adityakumar8279@gmail.com>
2024-10-12 17:12:43 +0530

    side bar

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,29 +1,61 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { NavLink } from "react-router-dom";
+import logo from "../img/LOGO.png"
+import '../App.css'
+import { faBars, faXmark } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+
+function Nav() {
+    return (
+        <>
+            <NavLink className={({ isActive }) => `py-0.5 md:py-3 px-4 md:px-1 border-s-2 md:border-s-0 hover:text-[#099e66] md:border-b-2 border-transparent hover:text-text-[#099e66] focus:outline-none text-lg  ${isActive ? "text-[#099e66] border-[#099e66]" : "text-white"}`} to="/">Home</NavLink>
+            <NavLink className={({ isActive }) => `py-0.5 md:py-3 px-4 md:px-1 border-s-2 md:border-s-0 hover:text-[#099e66] md:border-b-2 border-transparent hover:text-text-[#099e66] focus:outline-none text-lg  ${isActive ? "text-[#099e66] border-[#099e66]" : "text-white"}`} to="/about">About</NavLink>
+            <NavLink className={({ isActive }) => `py-0.5 md:py-3 px-4 md:px-1 border-s-2 md:border-s-0 hover:text-[#099e66] md:border-b-2 border-transparent hover:text-text-[#099e66] focus:outline-none text-lg  ${isActive ? "text-[#099e66] border-[#099e66]" : "text-white"}`} to="/resume">Resume</NavLink>
+            <NavLink className={({ isActive }) => `py-0.5 md:py-3 px-4 md:px-1 border-s-2 md:border-s-0 hover:text-[#099e66] md:border-b-2 border-transparent hover:text-text-[#099e66] focus:outline-none text-lg  ${isActive ? "text-[#099e66] border-[#099e66]" : "text-white"}`} to="/project">Project</NavLink>
+            <NavLink className={({ isActive }) => `py-0.5 md:py-3 px-4 md:px-1 border-s-2 md:border-s-0 hover:text-[#099e66] md:border-b-2 border-transparent hover:text-text-[#099e66] focus:outline-none text-lg  ${isActive ? "text-[#099e66] border-[#099e66]" : "text-white"}`} to="/contact">Contact</NavLink>
+        </>
+    )
+
+}
 
 
 function Header() {
+    const [ham, setHam] = useState(faBars);
+  const [content, setContent] = useState('hidden');
+
+  useEffect(() => {
+    if (ham === faXmark) {
+      setContent('block');
+    } else {
+      setContent('hidden');
+    }
+  }, [ham]);
+
+  const toggleSidebar = () => {
+    setHam(ham === faBars ? faXmark : faBars);
+  };
+
+
     return (
         <>
-            <header className="fixed flex flex-wrap md:justify-start md:flex-nowrap  z-50 w-full">
-                <nav className="mt-1 w-full bg-neutral-900/90 rounded-[2rem] mx-2 py-2.5 md:flex md:items-center md:justify-between md:py-0 md:px-4 md:mx-auto dark:bg-neutral-900/90 dark:border-[#099e66] ">
-                    <div className="px-4 md:px-0 flex justify-end items-center">
-                        <div className="md:hidden">
-                            <button type="button" className="hs-collapse-toggle flex justify-center items-center size-9 border border-[#099e66] text-white rounded-full hover:bg-[#099e66]/70 focus:outline-none focus:bg-[#099e66]/70 dark:border-[#099e66] dark:text-[#099e66] dark:hover:bg-[#099e66]/70 dark:focus:bg-[#099e66]/30" id="hs-navbar-header-floating-collapse" aria-expanded="false" aria-controls="hs-navbar-header-floating" aria-label="Toggle navigation" data-hs-collapse="#hs-navbar-header-floating">
-                                <svg className="hs-collapse-open:hidden shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><line x1="3" x2="21" y1="6" y2="6" /><line x1="3" x2="21" y1="12" y2="12" /><line x1="3" x2="21" y1="18" y2="18" /></svg>
-                                <svg className="hs-collapse-open:block hidden shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M18 6 6 18" /><path d="m6 6 12 12" /></svg>
-                            </button>
+            <header className="fixed z-50 w-full pt-3 flex justify-between">
+                <div className="">
+                    <img alt="" src={logo} width="100px" className="absolute top-0" />
+                </div>
+                <div className="block md:hidden px-7 text-xl py-3 ">
+                        <span><FontAwesomeIcon icon={ham} onClick={toggleSidebar} /></span>
+                        <div className={`absolute w-[120px] ${content} right-1 py-10 side_nav`}>
+                            <div className="flex-col flex gap-2">
+                                <Nav/>
+                            </div>
+                            
                         </div>
-                    </div>
 
-                    <div id="hs-navbar-header-floating" className="hidden hs-collapse overflow-hidden transition-all duration-300 basis-full grow md:block" aria-labelledby="hs-navbar-header-floating-collapse">
-                        <div className="flex flex-col md:flex-row md:items-center md:justify-end gap-2 md:gap-8 mt-3 md:mt-0 py-2 md:py-0 md:ps-7">
-                            <NavLink className={({ isActive }) => `py-0.5 md:py-3 px-4 md:px-1 border-s-2 md:border-s-0 hover:text-[#099e66] md:border-b-2 border-transparent hover:text-text-[#099e66] focus:outline-none text-lg  ${isActive ? "text-[#099e66] border-[#099e66]" : "text-white"}`} to="/">Home</NavLink>
-                            <NavLink className={({ isActive }) => `py-0.5 md:py-3 px-4 md:px-1 border-s-2 md:border-s-0 hover:text-[#099e66] md:border-b-2 border-transparent hover:text-text-[#099e66] focus:outline-none text-lg  ${isActive ? "text-[#099e66] border-[#099e66]" : "text-white"}`} to="/about">About</NavLink>
-                            <NavLink className={({ isActive }) => `py-0.5 md:py-3 px-4 md:px-1 border-s-2 md:border-s-0 hover:text-[#099e66] md:border-b-2 border-transparent hover:text-text-[#099e66] focus:outline-none text-lg  ${isActive ? "text-[#099e66] border-[#099e66]" : "text-white"}`} to="/resume">Resume</NavLink>
-                            <NavLink className={({ isActive }) => `py-0.5 md:py-3 px-4 md:px-1 border-s-2 md:border-s-0 hover:text-[#099e66] md:border-b-2 border-transparent hover:text-text-[#099e66] focus:outline-none text-lg  ${isActive ? "text-[#099e66] border-[#099e66]" : "text-white"}`} to="/project">Project</NavLink>
-                            <NavLink className={({ isActive }) => `py-0.5 md:py-3 px-4 md:px-1 border-s-2 md:border-s-0 hover:text-[#099e66] md:border-b-2 border-transparent hover:text-text-[#099e66] focus:outline-none text-lg  ${isActive ? "text-[#099e66] border-[#099e66]" : "text-white"}`} to="/contact">Contact</NavLink>
-                        </div>
+                </div>
+                <nav className="hidden  md:block px-5">
+                    <div className="  md:flex md:gap-6" >
+                        <Nav />
                     </div>
                 </nav>
             </header>
