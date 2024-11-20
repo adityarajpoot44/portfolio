2c2c2444d5970a7e26572639f4e3f632b2890508
Aditya Rajpoot <82802042+adityarajpoot44@users.noreply.github.com>
2024-11-20 17:48:36 +0000

    add loader

--- src/component/Myvideo.js
+++ src/component/Myvideo.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext } from 'react';
 import videoBg from '../assest/video/myVideo.mp4';
 import { DataContext } from '../App';
 
