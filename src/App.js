9e07c30de229ced291242a1fc54322ea1cc8e0f2
Aditya Rajpoot <82802042+adityarajpoot44@users.noreply.github.com>
2024-11-20 17:44:08 +0000

    add loader

--- src/App.js
+++ src/App.js
@@ -1,21 +1,31 @@
 
-import { createContext, useState } from 'react';
+import { createContext, useEffect, useState } from 'react';
 import './App.css';
 import Header from './component/Header.js';
 import Myvideo from './component/Myvideo.js';
 import { Outlet } from 'react-router-dom';
+import Loading from './component/pages/Loading.js';
 
 const DataContext = createContext();
 
 function App() {
   const [sharedData, setSharedData] = useState(true);
+  const [loading,setloading]=useState(true);
+
+  useEffect(()=>{
+    setTimeout(()=>{
+      setloading(false)
+    },5000)
+  })
+
   return (
     <>
-    <DataContext.Provider value={{sharedData,setSharedData}}>
+    {loading ? <Loading/>:<DataContext.Provider value={{sharedData,setSharedData}}>
       <Header />
       <Outlet/>
       <Myvideo />
       </DataContext.Provider>
+      }
     </>
   );
 }
---
+++ src/component/pages/Loading.js
@@ -0,0 +1,16 @@
+
+
+const Loading=()=>{
+    return(
+        <>
+            <div className="bg-black h-screen flex justify-center items-center">
+                <div className="loader h-10 ">
+
+                </div>
+
+            </div>
+        </>
+    )
+};
+
+export default Loading;
